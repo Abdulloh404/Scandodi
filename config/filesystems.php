ab5153a87476914304dbefc221d46eb6224cabba
Abdulloh404 <119023620+Abdulloh404@users.noreply.github.com>
2022-12-10 05:09:56 +0700

    Update filesystems.php

--- config/filesystems.php
+++ config/filesystems.php
@@ -48,18 +48,6 @@ return [
             'root' => storage_path('app'),
         ],
 
-        'permissions' => [
-            'file' => [
-                'public' => 0664,
-                'private' => 0600,
-            ],
-            'dir' => [
-                'public' => 0775,
-                'private' => 0700,
-            ],
-        ],
-
-
         'Assets' => [
             'driver' => 'local',
             'root' => storage_path('app/Assets'),
