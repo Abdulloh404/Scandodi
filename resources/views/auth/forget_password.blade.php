c2e32173d90e9cd4fd2217c502e456354e2f561e
Abdulloh404 <119023620+Abdulloh404@users.noreply.github.com>
2022-12-12 15:35:37 +0700

    Update forget_password.blade.php

--- resources/views/auth/forget_password.blade.php
+++ resources/views/auth/forget_password.blade.php
@@ -6,7 +6,7 @@
         <div class="container" style="display: flexbox;">
             <div class="row justify-content-center  align-items-center" style="height: 100vh">
                 <div class="col-md-5">
-                    <div class="authincation-content">
+                    <div class="authincation-content rounded-5">
                         <div class="row no-gutters">
                             <div class="col-xl-12">
                                 <div class="auth-form">
@@ -19,8 +19,9 @@
                                                    placeholder="{{trans('auth.email_ex')}}">
                                         </div>
                                         <div class="text-center">
-                                            <button type="submit"
-                                                    class="btn btn-primary btn-block">{{trans('auth.submit')}}</button>
+                                            <button
+                                                type="{{ json_decode(get_settings('site_setting')) && isset(json_decode(get_settings('site_setting'))->recaptcha_site_key) ? 'button' : 'submit' }}"
+                                                class="btn btn-outline-secondary btn-block signIn">{{trans('auth.submit')}}</button>
                                         </div>
                                     </form>
                                 </div>
