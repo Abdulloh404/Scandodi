8c3a4bf18d638ec53095e11035b130434d170fe3
Abdulloh404 <adlforgithub@gmail.com>
2022-12-14 14:57:05 +0700

    Update content-price.blade.php

--- resources/views/components/content-price.blade.php
+++ resources/views/components/content-price.blade.php
@@ -4,69 +4,69 @@
     <div class="container p-xl-4 p-lg-4 p-md-4 p-0 py-3">
         <div class="row g-4">
             <div class="d-grid col-xl-3 col-lg-3 col-md-6 col-12">
-                <a href="{{route('content-price')}}" class="d-grid">
-                <div class="card border-0 wow fadeInRight" data-wow-delay="0.2s">
-                    <img src="uploads/img-price-1.png" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">Increase Your Revenue More Than 30%</h5>
-                        <p class="card-text">
-                        <ul>
-                            <li>Increased Table Turnover</li>
-                            <li>Increased Ticket Sizes</li>
-                            <li>Increased Tips</li>
-                        </ul>
-                        </p>
+                <a href="{{ route('content-price') }}" class="d-grid">
+                    <div class="card border-0 wow fadeInRight" data-wow-delay="0.2s">
+                        <img src="uploads/img-price-1.png" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Increase Your Revenue More Than 30%</h5>
+                            <p class="card-text">
+                            <ul>
+                                <li>Increased Table Turnover</li>
+                                <li>Increased Ticket Sizes</li>
+                                <li>Increased Tips</li>
+                            </ul>
+                            </p>
+                        </div>
                     </div>
-                </div>
             </div></a>
             <div class="d-grid col-xl-3 col-lg-3 col-md-6 col-12">
-                <a href="{{route('content-price')}}" class="d-grid">
-                <div class="card border-0 bg-color wow fadeInRight" data-wow-delay="0.3s">
-                    <img src="uploads/img-price-2.png" class="card-img-top" alt="...">
-                    <div class="card-body text-white">
-                        <h5 class="card-title">Decrease Operational Costs by 30%</h5>
-                        <p class="card-text">
-                        <ul>
-                            <li>Eliminate Human Errors</li>
-                            <li>Increased Labor Productivity</li>
-                            <li>Low Maintenance Cost</li>
-                        </ul>
-                        </p>
+                <a href="{{ route('content-price') }}" class="d-grid">
+                    <div class="card border-0 bg-color wow fadeInRight" data-wow-delay="0.3s">
+                        <img src="uploads/img-price-2.png" class="card-img-top" alt="...">
+                        <div class="card-body text-white">
+                            <h5 class="card-title">Decrease Operational Costs by 30%</h5>
+                            <p class="card-text">
+                            <ul>
+                                <li>Eliminate Human Errors</li>
+                                <li>Increased Labor Productivity</li>
+                                <li>Low Maintenance Cost</li>
+                            </ul>
+                            </p>
+                        </div>
                     </div>
-                </div>
             </div></a>
 
             <div class="d-grid col-xl-3 col-lg-3 col-md-6 col-12">
-                <a href="{{route('content-price')}}" class="d-grid">
-                <div class="card border-0 wow fadeInRight" data-wow-delay="0.4s">
-                    <img src="uploads/img-price-3.png" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">Gain More Control Over Your Business</h5>
-                        <p class="card-text">
-                        <ul>
-                            <li>Real-Time Data</li>
-                            <li>Data Analytics for Smarter Decisions</li>
-                            <li>Update with One Clickt</li>
-                        </ul>
-                        </p>
+                <a href="{{ route('content-price') }}" class="d-grid">
+                    <div class="card border-0 wow fadeInRight" data-wow-delay="0.4s">
+                        <img src="uploads/img-price-3.png" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Gain More Control Over Your Business</h5>
+                            <p class="card-text">
+                            <ul>
+                                <li>Real-Time Data</li>
+                                <li>Data Analytics for Smarter Decisions</li>
+                                <li>Update with One Clickt</li>
+                            </ul>
+                            </p>
+                        </div>
                     </div>
-                </div>
             </div></a>
             <div class="d-grid col-xl-3 col-lg-3 col-md-6 col-12">
-                <a href="{{route('content-price')}}" class="d-grid">
-                <div class="card border-0 bg-color wow fadeInRight" data-wow-delay="0.5s">
-                    <img src="uploads/img-price-2.png" class="card-img-top" alt="...">
-                    <div class="card-body text-white">
-                        <h5 class="card-title">Decrease Operational Costs by 30%</h5>
-                        <p class="card-text">
-                        <ul>
-                            <li>Eliminate Human Errors</li>
-                            <li>Increased Labor Productivity</li>
-                            <li>Low Maintenance Cost</li>
-                        </ul>
-                        </p>
+                <a href="{{ route('content-price') }}" class="d-grid">
+                    <div class="card border-0 bg-color wow fadeInRight" data-wow-delay="0.5s">
+                        <img src="uploads/img-price-2.png" class="card-img-top" alt="...">
+                        <div class="card-body text-white">
+                            <h5 class="card-title">Decrease Operational Costs by 30%</h5>
+                            <p class="card-text">
+                            <ul>
+                                <li>Eliminate Human Errors</li>
+                                <li>Increased Labor Productivity</li>
+                                <li>Low Maintenance Cost</li>
+                            </ul>
+                            </p>
+                        </div>
                     </div>
-                </div>
             </div></a>
         </div>
     </div>
