30565491410c16908177956875516878400c02fc
Abdulloh404 <adlforgithub@gmail.com>
2022-12-16 11:38:01 +0700

    Update PriceController.php

--- app/Http/Controllers/PriceController.php
+++ app/Http/Controllers/PriceController.php
@@ -3,16 +3,25 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Events\SendMail;
+use App\Models\CallWaiter;
+use App\Models\CustomMenu;
+use App\Models\Item;
+use App\Models\Menu;
+use App\Models\Order;
+use App\Models\OrderDetails;
+use App\Models\Plan;
+use App\Models\Restaurant;
 class PriceController extends Controller
 {
     function showprice(){
-        return view('price.index');
+        $data['plans'] = Plan::where(['status' => 'active'])->where('id', '!=', 1)->get();
+        $modules = modules_status('MultiRestaurant');
+        if ($modules){
+            return redirect()->route('multirestaurant::index');
+        }else{
+            return view('price.index', $data);
+        }
     }
 
-    public function getUser()
-    {
-        $plan = 3;
-        return view('auth.login');
-    }
 }
