472b588de76f2f60c2fac3025198b1d12151f7e3
Abdulloh404 <adlforgithub@gmail.com>
2022-12-19 15:19:38 +0700

    UPDATE อีกแล้ว

--- resources/views/plans/plan_table.blade.php
+++ resources/views/plans/plan_table.blade.php
@@ -110,7 +110,21 @@
 
                                                 </div>
                                             </td>
-                                            <td>{{ $plan->image }}</td>
+                                            <td>
+                                                <div class="view">
+                                                    <a href="{{ asset('uploads-images/' . $plan->image) }}">
+                                                        <button type="button" class="btn btn-success light sharp">
+                                                            <svg xmlns="http://www.w3.org/2000/svg" color="#2bc155"
+                                                                width="20" height="20" fill="currentColor"
+                                                                class="bi bi-eye-fill" viewBox="0 0 16 16">
+                                                                <path d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0z" />
+                                                                <path
+                                                                    d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8zm8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7z" />
+                                                            </svg>
+
+                                                        </button></a>
+                                                </div>
+                                            </td>
                                         </tr>
                                     @endforeach
                                 @endif
